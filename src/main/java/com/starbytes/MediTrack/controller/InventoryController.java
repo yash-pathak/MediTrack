c385c7ddf1d362f86f81523529e6540e6c2edc8c
yash-pathak <yashpathak1495@gmail.com>
2025-03-07 10:36:09 +0530

    test commit

--- src/main/java/com/starbytes/MediTrack/controller/InventoryController.java
+++ src/main/java/com/starbytes/MediTrack/controller/InventoryController.java
@@ -1,8 +1,14 @@
 package com.starbytes.MediTrack.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InventoryController {
+    
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
 
 }
